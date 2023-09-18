a97df18b4c60dd45c6d273afd70afa51086f1061
Marie-Hélène <mh.roussel@hotmail.fr>
2023-09-18 12:22:03 +0200

    a

--- FrontEnd/assets/script/script.js
+++ FrontEnd/assets/script/script.js
@@ -1,2 +1,9 @@
-const response = await fetch("http:/localhost:5678/api/works") 
+async function loadWorks() {
+    const response = await fetch("http:/localhost:5678/api/works") 
 console.log(response)
+const responseFinale = await response.json()
+console.log(responseFinale)
+}
+
+loadWorks()
+
