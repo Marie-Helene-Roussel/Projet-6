16f0927f092a3309e7b3dc95f3fe052cf242c805
Marie-Hélène <mh.roussel@hotmail.fr>
2023-09-25 11:32:31 +0200

    e

--- FrontEnd/assets/script/config.js
+++ FrontEnd/assets/script/config.js
@@ -1,8 +1,8 @@
 async function loadWorks() {
     const response = await fetch("http:/localhost:5678/api/works") 
 console.log(response)
-const responseFinale = await response.json()
-console.log(responseFinale)
+const responseWork = await response.json()
+console.log(responseWork)
 }
 
 loadWorks()
