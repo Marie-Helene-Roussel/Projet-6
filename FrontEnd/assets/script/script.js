8743eba6eac2320ae0071e613d9fa5f86ae22ca8
Marie-Hélène <mh.roussel@hotmail.fr>
2023-09-29 13:36:22 +0200

    Vignettes affichés.

--- FrontEnd/assets/script/script.js
+++ FrontEnd/assets/script/script.js
@@ -12,3 +12,25 @@ console.log(responseWork [i])
 
 loadWorks()
 
+// création du premier élément html pour la première vignette. Fonction pour l'affichage d'une image!
+
+function afficherFigureGallery(){
+    const divGallery = document.querySelector(".gallery")
+    const figure = document.createElement("figure")
+    const img = document.createElement("img")
+    const figcaption = document.createElement("figcaption")
+
+   img.src = "http://localhost:5678/images/abajour-tahina1651286843956.png"
+   img.alt = "Un abajour"
+   figcaption.innerText = "Abajour Tahina"
+
+   divGallery.appendChild(figure)
+   figure.appendChild(img)
+   figure.appendChild(figcaption)
+   
+   console.log(img)
+
+} 
+
+afficherFigureGallery()
+
