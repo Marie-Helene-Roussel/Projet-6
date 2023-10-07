b6231082cf5e87a60dba4048819ee5d9b0e275f3
Marie-Hélène <mh.roussel@hotmail.fr>
2023-10-07 09:09:53 +0200

    création du filtre avant de le mettre en fonctionnement!

--- FrontEnd/assets/script/script.js
+++ FrontEnd/assets/script/script.js
@@ -1,36 +1,49 @@
-async function loadWorks() {
-    const response = await fetch("http:/localhost:5678/api/works") 
-console.log(response)
-const responseWork = await response.json()
-console.log(responseWork)
+function loadWorks() {
+    // fonction pour retourner le résultat sans avoir à attendre
+    return fetch("http:/localhost:5678/api/works")
+        // exécute uniquement s'il a la réponse
+        .then((responseWork) => responseWork.json())
+        // retourne tous les élément et recherche chaque élément un à un.
+        .then((elements) => elements.forEach(element => {
+            // affiche les images défini dans la fonction et le titre pour chaque élément
+            afficherFigureGallery(element.imageUrl, element.title)
 
-for (let i = 0 ; i < responseWork.length; i++)
-{
-console.log(responseWork [i])
-}
+            console.log(elements)
+        }))
 }
 
 loadWorks()
 
 // création du premier élément html pour la première vignette. Fonction pour l'affichage d'une image!
 
-function afficherFigureGallery(){
+function afficherFigureGallery(url, title) {
     const divGallery = document.querySelector(".gallery")
     const figure = document.createElement("figure")
     const img = document.createElement("img")
     const figcaption = document.createElement("figcaption")
+    // les paramètres à afficher qui sont définie par element ligne 10.
+    img.src = url
+    img.alt = title
+    figcaption.innerText = title
 
-   img.src = "http://localhost:5678/images/abajour-tahina1651286843956.png"
-   img.alt = "Un abajour"
-   figcaption.innerText = "Abajour Tahina"
+    divGallery.appendChild(figure)
+    figure.appendChild(img)
+    figure.appendChild(figcaption)
 
-   divGallery.appendChild(figure)
-   figure.appendChild(img)
-   figure.appendChild(figcaption)
-   
-   console.log(img)
+    console.log(img)
 
-} 
+}
 
-afficherFigureGallery()
+// fonctions qui vont afficher les boutons de filtre de la maquette. avec un fetch, en remplaçant le lien par catégorie
 
+//fonctions pour le filtre.
+
+//une fonction pour l'affichage
+
+
+
+
+
+
+
+//une fonction pour le fonctionnement du filtre
