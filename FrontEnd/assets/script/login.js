79fb5b374ef85901916456770508c05d7adda276
Marie-Hélène <mh.roussel@hotmail.fr>
2023-10-14 18:39:52 +0200

    fini le login

--- FrontEnd/assets/script/login.js
+++ FrontEnd/assets/script/login.js
@@ -1,43 +1,28 @@
-
-
-//partie pour le login
-
-
-
-//La fonction pour vérifier que le mot de passe et bon et envoyer un résultat
-function login(e) {
-
-    //preventDefault pour empêcher submit d'avoir un comportement par défaut
-    e.preventDefault()
-    // lorsque le mail et mot de passe est correct
+function connect(event) {
+    event.preventDefault();
     const logInput = {
-        email: document.querySelector("#email").value,
-        motDePasse: document.querySelector("#motDePasse").value
-    }
-    console.log(logInput)
-    const id = JSON.stringify(logInput)
-    console.log(id)
+        email: document.querySelector('#email').value,
+        password: document.querySelector('#motDePasse').value
+    };
+    console.log(logInput);
+    const id = JSON.stringify(logInput);
     fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: id
     })
-    
-        .then(responseLogin => responseLogin.json())
-        .then((responseLog) => {
-            if (!responseLog.token) {
-                alert("Le email ou mot de passe est incorrect!!")
+        .then(resp => resp.json())
+        .then((response) => {
+            if (!response.token) {
+                alert("Identifiant ou mot de passe incorrect");
                 return;
             }
-            const token = JSON.stringify(responseLog.token)
-            window.localStorage.setItem("keyToken", token)
-            window.location = "./index.html"
 
+            const token = JSON.stringify(response.token);
+            window.localStorage.setItem("token", token);
+            window.location = "./index.html";
         })
+};
 
-
-}
-// Pour quand on clique sur le bouton se connecter
-const loginForm = document.querySelector(".loginForm")
-loginForm.addEventListener("submit", (e) => login(e))
-
+const form = document.querySelector(".loginForm")
+form.addEventListener("submit", (event) => connect(event))
